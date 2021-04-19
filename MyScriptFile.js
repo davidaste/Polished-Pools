45b13437902e6a58512ee16c59ba301267b0d319
David Aste <78303927+davidaste@users.noreply.github.com>
2021-04-19 02:27:03 -0700

    first commit

---
+++ MyScriptFile.js
@@ -0,0 +1,6 @@
+function addMessage() {
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    document.getElementById('message').innerHTML = 'It is currently ' + hours + ':' + minutes;
+    }
